9a1915bcc78fc3b2b0b105b126546794a090bcc1
parksuejin1026 <suejin1026suejin@gmail.com>
2026-06-02 16:22:32 +0900

    13주차 두 번째 과제

--- Week13/Week13_Assi_2/app/src/main/java/com/cookandroid/week13_assi_2/MainActivity.java
+++ Week13/Week13_Assi_2/app/src/main/java/com/cookandroid/week13_assi_2/MainActivity.java
@@ -2,7 +2,13 @@ package com.cookandroid.week13_assi_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("영화 선호도 투표");
+
+        // 투표 결과 배열
+        final int voteCount[] = new int[9];
+
+        for (int i = 0 ; i < 9 ; i++)
+            voteCount[i] = 0; // 전부 0표로 SET
+
+        ImageView image[] = new ImageView[9];
+        // 이미지뷰 아이디값 저장
+        Integer imageId[] = {   R.id.iv1, R.id.iv2, R.id.iv3,
+                                R.id.iv4, R.id.iv5, R.id.iv6,
+                                R.id.iv7, R.id.iv8, R.id.iv9 };
+        // 이미지 이름 저장
+        final String imgName[] = {  "독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀",
+                                    "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매",
+                                    "피아노 레슨", "피아노 앞의 소녀들", "해변에서"};
+
+        for (int i = 0; i < imageId.length; i++) {
+            final int index;
+            index = i;
+            image[index] = (ImageView) findViewById(imageId[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), imgName[index] + ": 총 " + voteCount[index] + "표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        Button btnFinish = (Button) findViewById(R.id.btnResult);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("VoteCount", voteCount);
+                intent.putExtra("ImageName", imgName);
+                startActivity(intent);
+            }
+        });
+
     }
 }
